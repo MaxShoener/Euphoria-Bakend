217ee9f741cc5d775bd8a439fc9d904186dd18d8
MaxShoener <meshoener@gmail.com>
2025-09-26 00:16:32 -0400

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const path = require("path");
-const { chromium } = require("playwright"); // headless browser
+const { chromium } = require("playwright-chromium");
 
 const app = express();
 const PORT = process.env.PORT || 10000;
